419218fb10bcc4e2be8a5ce91c4968abf35ea1a6
kimbukky <ennykim03@gmail.com>
2022-09-12 09:46:48 -0700

    Write a program that prints all possible different combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+# include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, l;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 49; j < 58; j++)
+		{
+			for (l = 50; l < 58; l++)
+			{
+				if (l > j && j > i)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(l);
+					if (i != 55 || j != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
