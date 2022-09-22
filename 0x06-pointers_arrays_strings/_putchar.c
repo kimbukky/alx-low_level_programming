3bfa7e2f0a2261b0e9a0c499e48b83280f22aebc
kimbukky <ennykim03@gmail.com>
2022-09-22 14:14:21 -0700

    Write a function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+
+/**
+ * _putchar- write the character c to stdout
+ * @c:the character to print
+ * Return: success 1
+ * On error, -1 is returned, and  error is set
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
