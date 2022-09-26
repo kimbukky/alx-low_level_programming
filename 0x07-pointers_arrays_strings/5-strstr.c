bc9c1fc1b0b576cc2a311da3c40cc1537b3c3979
kimbukky <ennykim03@gmail.com>
2022-09-26 15:19:11 -0700

    Create a file that contains the password for the crackme2 executable and ammend 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,7 +10,7 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
-	
+
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
@@ -23,4 +23,3 @@ char *_strstr(char *haystack, char *needle)
 	}
 	return (NULL);
 }
-
